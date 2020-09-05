fc29452b735348e98914603ef988ebd9ffcf7c2c
YKW93 <rhkddn1657@gmail.com>
2020-09-05 16:16:46 +0900

    Added Sample code

--- app/src/main/java/com/wayne/constraintradiogroup/demo/MainActivity.kt
+++ app/src/main/java/com/wayne/constraintradiogroup/demo/MainActivity.kt
@@ -2,10 +2,35 @@ package com.wayne.constraintradiogroup.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import com.wayne.constraintradiogroup.ConstraintRadioGroup
+import com.wayne.constraintradiogroup.OnCheckedChangeListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        crg_custom.checkedChangeListener = object : OnCheckedChangeListener {
+            override fun onCheckedChanged(
+                group: ConstraintRadioGroup,
+                checkedButton: CompoundButton
+            ) {
+                // TODO
+//                (checkedButton as RadioButton)
+            }
+        }
+
+        crg_check_box.checkedChangeListener = object : OnCheckedChangeListener {
+            override fun onCheckedChanged(
+                group: ConstraintRadioGroup,
+                checkedButton: CompoundButton
+            ) {
+                // TODO
+//                (checkedButton as CheckBox)
+            }
+
+        }
     }
 }
